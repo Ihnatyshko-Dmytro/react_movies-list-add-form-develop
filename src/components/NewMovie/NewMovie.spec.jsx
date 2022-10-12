0ffbbca6ac3b5e296254f8126d03e6b655c4d2a1
Ihnatyshko Dmytro <110336103+Ihnatyshko-Dmytro@users.noreply.github.com>
2022-10-12 13:35:07 +0300

    Add files via upload

---
+++ src/components/NewMovie/NewMovie.spec.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import React from 'react';
+import { mount } from '@cypress/react';
+import { NewMovie } from './NewMovie';
+// import { onAdd } from './NewMovie';
+
+describe('Button', () => {
+  const form = {
+    fillAllFields() {
+      return cy.getByDataCy('form-title')
+        .type('movie-title')
+        && cy.getByDataCy('form-description')
+          .type('movie-description')
+        && cy.getByDataCy('form-imgUrl')
+          .type('movie-img')
+        && cy.getByDataCy('form-imdbUrl')
+          .type('movie-url')
+        && cy.getByDataCy('form-imdbId')
+          .type('movie-id');
+    },
+  };
+
+  it('should call the onAdd function', () => {
+    const onAdd = cy.stub();
+
+    // eslint-disable-next-line react/jsx-filename-extension
+    mount(<NewMovie onAdd={onAdd} />);
+
+    form.fillAllFields();
+
+    cy.get('button')
+      .click()
+      .then(() => {
+        expect(onAdd).to.be.called;
+      });
+  });
+});
